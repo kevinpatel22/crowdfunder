b14654d8d2d76e87fe45d2761ce4cd1b7cc0c580
ynjacobs <ytj64611@gamil.com>
2019-06-06 16:09:55 -0400

    set up original design

--- crowdfunder_project/crowdfunder_project/models.py
+++ crowdfunder_project/crowdfunder_project/models.py
@@ -9,10 +9,11 @@ class Project(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     budget = models.IntegerField()
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='project')
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='projects')
     
 
 
 class Backer(models.Model):
     name = models.CharField(max_length=255)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='backer')
+    projects = models.ManyToManyField(Project, on_delete=models.CASCADE, related_name='backers')
+    
--- crowdfunder_project/crowdfunder_project/urls.py
+++ crowdfunder_project/crowdfunder_project/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from crowdfunder_project.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home, name='home_page'),
     path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout')
+    path('logout/', logout_view, name='logout'),
     path('signup/', signup, name='signup'),
 ]
