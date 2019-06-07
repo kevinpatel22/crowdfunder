5c0150449a43b41fbff3547341739076d5e01f40
hassanashmawy <hassan.ashmawy@gmail.com>
2019-06-07 17:54:32 -0400

    Co-authored-by: kevinpatel22 <mail.kevinpatel@gmail.com> Co-authored-by: coreycrosby <ccc_13@live.ca> Co-authored-by: Josh Teneycke <josh.teneycke@gmail.com>

--- crowdfunder_project/crowdfunder_project/admin.py
+++ crowdfunder_project/crowdfunder_project/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from crowdfunder_project.models import Category
 from django.contrib.auth.models import User
+from .models import * 
+
 
 admin.site.register(Category)
+admin.site.register(Project)
+admin.site.register(Comment)
+admin.site.register(Reward)
+admin.site.register(Donation)
